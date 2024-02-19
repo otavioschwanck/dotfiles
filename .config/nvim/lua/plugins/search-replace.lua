401b194d70217c79b991a53a707eb246edf6324e
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2024-02-19 18:56:51 +0000

    Manual Sync

--- .config/nvim/lua/plugins/search-replace.lua
+++ .config/nvim/lua/plugins/search-replace.lua
@@ -7,8 +7,7 @@ return {
 
       local opts = {}
 
-      vim.api.nvim_set_keymap("v", "gq", "<CMD>SearchReplaceSingleBufferVisualSelection<CR>", opts)
-      vim.api.nvim_set_keymap("v", "g!q", "<CMD>SearchReplaceWithinVisualSelection<CR>", opts)
+      vim.api.nvim_set_keymap("v", "gq", "<CMD>SearchReplaceWithinVisualSelection<CR>", opts)
       vim.api.nvim_set_keymap("v", "gQ", "<CMD>SearchReplaceWithinVisualSelectionCWord<CR>", opts)
 
       vim.api.nvim_set_keymap("n", "gq", "<CMD>SearchReplaceSingleBufferCWord<CR>", opts)
