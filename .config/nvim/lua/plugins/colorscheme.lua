6b545e9cf053dfa3c43ead0c25f7371b6bd11995
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2024-03-04 14:03:18 +0000

    Manual Sync

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -13,14 +13,14 @@ return {
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_background = "soft"
 
       -- create autocmd for colorscheme
       vim.api.nvim_create_autocmd("ColorScheme", {
         pattern = "*",
         callback = function()
           vim.api.nvim_set_hl(0, "NormalFloat", { link = "NormalNC" })
-          vim.api.nvim_set_hl(0, "FloatBorder", { link = "NormalNC" })
+          vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#89bf82" })
         end,
       })
 
