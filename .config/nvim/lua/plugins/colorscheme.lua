b10356289ee9c03f96d63b5caaff3fb15746a308
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2024-06-20 15:25:57 +0100

    Manual Sync

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,29 +1,29 @@
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		lazy = false,
-		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha",
-				custom_highlights = function()
-					return {
-						NormalFloat = { link = "Normal" },
-					}
-				end,
-				integrations = {
-					cmp = true,
-					gitsigns = true,
-					nvimtree = true,
-					treesitter = true,
-					notify = true,
-					telescope = {
-						enabled = true,
-						style = "nvchad",
-					},
-				},
-			})
-			vim.cmd([[colorscheme catppuccin]])
-		end,
-	},
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        custom_highlights = function()
+          return {
+            NormalFloat = { link = "Normal" },
+          }
+        end,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = true,
+          telescope = {
+            enabled = true,
+            style = "nvchad",
+          },
+        },
+      })
+      vim.cmd([[colorscheme catppuccin]])
+    end,
+  },
 }
