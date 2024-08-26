e0d7aa6fe746e9f82f44d7b632da0d7e837b95d1
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2024-08-26 14:32:12 +0100

    Manual Sync

--- .config/nvim/lua/plugins/codeium.lua
+++ .config/nvim/lua/plugins/codeium.lua
@@ -1,21 +1,25 @@
-return {
-	{
-		"Exafunction/codeium.vim",
-		event = "InsertEnter",
-		commit = "a1c3d6b369a18514d656dac149de807becacbdf7",
-		config = function()
-			vim.g.codeium_disable_bindings = 1
-			vim.g.codeium_filetypes = { TelescopePrompt = false }
+if true then
+  return {}
+end
 
-			vim.keymap.set("i", "<C-j>", function()
-				return vim.fn["codeium#Accept"]()
-			end, { expr = true })
-			vim.keymap.set("i", "<C-b>", function()
-				return vim.fn["codeium#CycleCompletions"](1)
-			end, { expr = true })
-			vim.keymap.set("i", "<C-]>", function()
-				return vim.fn["codeium#Clear"]()
-			end, { expr = true })
-		end,
-	},
+return {
+  {
+    "Exafunction/codeium.vim",
+    event = "InsertEnter",
+    commit = "a1c3d6b369a18514d656dac149de807becacbdf7",
+    config = function()
+      vim.g.codeium_disable_bindings = 1
+      vim.g.codeium_filetypes = { TelescopePrompt = false }
+
+      vim.keymap.set("i", "<C-j>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true })
+      vim.keymap.set("i", "<C-b>", function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end, { expr = true })
+      vim.keymap.set("i", "<C-]>", function()
+        return vim.fn["codeium#Clear"]()
+      end, { expr = true })
+    end,
+  },
 }
---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        panel = {
+          enabled = true,
+          auto_refresh = true,
+          keymap = {
+            jump_prev = "[[",
+            jump_next = "]]",
+            accept = "<CR>",
+            refresh = "gr",
+            open = "<M-CR>",
+          },
+          layout = {
+            position = "bottom",
+            ratio = 0.4,
+          },
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          hide_during_completion = true,
+          debounce = 75,
+          keymap = {
+            accept = "<C-j>",
+            accept_word = "<C-l>",
+            accept_line = false,
+            next = "<C-b>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        },
+      })
+    end,
+  },
+}
