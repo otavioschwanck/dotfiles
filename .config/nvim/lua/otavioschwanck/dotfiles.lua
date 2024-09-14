04551c7c6ce710798d670d09b23e0ff8d111f1d9
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2024-09-14 15:22:40 +0100

    skhdrc

--- .config/nvim/lua/otavioschwanck/dotfiles.lua
+++ .config/nvim/lua/otavioschwanck/dotfiles.lua
@@ -7,7 +7,8 @@ local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 
 local dotfiles = {
-  { path = "~/.config/i3/config",                              display = "i3" },
+  { path = "~/.config/bspwm/bspwmrc",                          display = "bspwm" },
+  { path = "~/.config/sxhkd/sxhkdrc",                          display = "sxhkdrc" },
   { path = "~/.config/alacritty/alacritty.toml",               display = "Alacritty" },
   { path = "~/.zshrc",                                         display = "zshrc" },
   { path = "~/.zsh_aliases",                                   display = "zshrc aliases" },
