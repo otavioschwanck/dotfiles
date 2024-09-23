d0bcd5039b6fdccb37d511a9ef531aca25591a74
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2024-09-23 18:13:30 +0100

    Manual Sync

--- .config/nvim/lua/plugins/codeium.lua
+++ .config/nvim/lua/plugins/codeium.lua
@@ -2,11 +2,13 @@ return {
   {
     "Exafunction/codeium.vim",
     event = "InsertEnter",
-    commit = "a1c3d6b369a18514d656dac149de807becacbdf7",
     config = function()
       vim.g.codeium_disable_bindings = 1
       vim.g.codeium_filetypes = { TelescopePrompt = false }
 
+      vim.keymap.set("i", "<C-f>", function()
+        return vim.fn["codeium#AcceptNextWord"]()
+      end, { expr = true })
       vim.keymap.set("i", "<C-j>", function()
         return vim.fn["codeium#Accept"]()
       end, { expr = true })
