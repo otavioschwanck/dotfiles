cbacc66236416c1a3b916b53f922712bc1c8e2f1
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2024-11-13 13:35:10 +0000

    Manual Sync

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -39,6 +39,47 @@ return {
         color = { fg = catppuccin_colors.pink },
       }
 
+      local filename_with_icon = require('lualine.components.filename'):extend()
+      filename_with_icon.apply_icon = require('lualine.components.filetype').apply_icon
+      filename_with_icon.icon_hl_cache = {}
+
+
+      local conditions = {
+        buffer_not_empty = function()
+          return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
+        end,
+      }
+
+      local function count(base, pattern)
+        return select(2, string.gsub(base, pattern, ''))
+      end
+
+      local function shorten_path(path, sep)
+        -- ('([^/])[^/]+%/', '%1/', 1)
+        return path:gsub(string.format('([^%s])[^%s]+%%%s', sep, sep, sep), '%1' .. sep, 1)
+      end
+
+      local function path()
+        local dir = vim.fn.expand("%:p:h")
+
+        if dir == vim.fn.getcwd() then
+          return " Project Root"
+        else
+          local windwidth = vim.go.columns or vim.fn.winwidth(0)
+          local estimated_space_available = windwidth - 30
+
+          local data = vim.fn.fnamemodify(dir, ":~:.")
+          for _ = 0, count(data, '/') do
+            if windwidth <= 84 or #data > estimated_space_available then
+              data = shorten_path(data, '/')
+            end
+          end
+
+          return " " .. data
+        end
+      end
+
+
       require("lualine").setup({
         options = {
           disabled_filetypes = {
@@ -51,8 +92,8 @@ return {
         },
         sections = {
           lualine_a = { "mode" },
-          lualine_b = { "filename", arrow_module },
-          lualine_c = { "diagnostics", "diff" },
+          lualine_b = { { filename_with_icon, color = { gui = 'bold' } }, { path, color = { fg = catppuccin_colors.pink } } },
+          lualine_c = { arrow_module, "diagnostics", "diff" },
           lualine_x = { open_terms, "branch" },
           lualine_y = { "progress" },
           lualine_z = { "location" },
