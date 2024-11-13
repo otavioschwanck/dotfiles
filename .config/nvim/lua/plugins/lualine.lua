5f42623e2d00e07cf49a573c2a61266989f0f6a8
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2024-11-13 13:22:41 +0000

    Manual Sync

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -47,7 +47,7 @@ return {
           section_separators = { left = '', right = '' },
           component_separators = { left = '', right = '' },
           theme = theme,
-          globalstatus = false,
+          globalstatus = true,
         },
         sections = {
           lualine_a = { "mode" },
