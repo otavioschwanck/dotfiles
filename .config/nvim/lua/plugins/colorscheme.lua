3a00f5d5fd9325ca817f17bb50a30a7daa0e4eb8
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2024-11-14 12:35:23 +0000

    Manual Sync

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -6,7 +6,6 @@ return {
     config = function()
       require("catppuccin").setup({
         flavour = "mocha",
-        transparent_background = true,
         custom_highlights = function()
           return {
             NormalFloat = { link = "Normal" },
