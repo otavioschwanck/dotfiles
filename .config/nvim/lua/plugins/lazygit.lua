8f0f65f3b4e190f1c55ef8f9c6bec0339957cd0b
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2025-02-18 13:28:16 +0000

    Manual Sync

--- .config/nvim/lua/plugins/lazygit.lua
+++ .config/nvim/lua/plugins/lazygit.lua
@@ -1,8 +1,19 @@
 return {
   {
-    'voldikss/vim-floaterm',
-    keys = {
-      { "<leader>gg", "<cmd>FloatermNew --width=0.95 --height=0.95 lazygit<CR>", desc = "Lazygit" },
-    }
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    config = function()
+      require("toggleterm").setup({ size = 120 })
+
+      local Terminal = require('toggleterm.terminal').Terminal
+      local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true, height = 100 })
+
+      function _lazygit_toggle()
+        lazygit:toggle()
+      end
+
+      vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>",
+        { noremap = true, silent = true, desc = "LazyGit" })
+    end
   },
 }
