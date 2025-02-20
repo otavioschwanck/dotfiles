80be21d8d3667dc3c50e9387ba7d6e2f95052e09
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2025-02-20 12:22:42 +0000

    Manual Sync

--- .config/nvim/lua/plugins/lazygit.lua
+++ .config/nvim/lua/plugins/lazygit.lua
@@ -1,19 +1,10 @@
 return {
   {
-    'akinsho/toggleterm.nvim',
-    version = "*",
-    config = function()
-      require("toggleterm").setup({ size = 120 })
-
-      local Terminal = require('toggleterm.terminal').Terminal
-      local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true, height = 100 })
-
-      function _lazygit_toggle()
-        lazygit:toggle()
-      end
-
-      vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>",
-        { noremap = true, silent = true, desc = "LazyGit" })
-    end
+    "kdheepak/lazygit.nvim",
+    cmd = "LazyGit",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    }
   },
 }
